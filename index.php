a7f7390d3a322feffdfeca6f97c30b6bbff83125
ramwadhwa1031 <48644649+ramwadhwa1031@users.noreply.github.com>
2020-04-29 18:56:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
-<?php
-  $host='db';
-  $user= 'ram';
-  $password= 'ram';
-  $db= 'mydb';
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  $conn=new mysqli($host, $user,$password, $db);
-  if($conn ->connect_error){
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Docker Project</title>
+  </head>
+  <body>
+    	<?php
+  	$host='db';
+  	$user= 'ram';
+  	$password= 'ram';
+  	$db= 'mydb';
+
+  	$conn=new mysqli($host, $user,$password, $db);
+  	if($conn ->connect_error){?>
+	<div class="alert alert-danger" role="alert">
+	<?php
 	echo 'connection failed'.$conn->connect_error;
-	}
-  echo 'successufully connected to mysqli';
+	}?>
+<?php
+	<div class="alert alert-success" role="alert">
+	<?php
+  		echo '<h1>successufully connected to mysqli</h1>';
 ?>
+	<div class="jumbotron">
+  <h1 class="display-4">PHP USE CASE</h1>
+  <p class="lead">PHP is an interpreted language. This means that when you make changes to your source code you can immediately test these changes, without first needing to compile your source code into binary form. Skipping the compilation step makes the development process much faster.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+  </body>
+</html>
+
